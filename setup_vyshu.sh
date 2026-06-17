0a777ce3a18440dc1bdc28007b19576cf51b37e5
Manikanta Teja Swaroop Arni <manikantatejaswarooparni@gmail.com>
2026-06-18 02:11:32 +0530

    Delete setup_vyshu.sh

--- setup_vyshu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Creating Vyshu AI V4 files..."
-
-# This script writes all files
-# Run: bash setup_vyshu.sh
-
-echo "Done! Run: git add . && git commit -m 'Vyshu V4' && git push"
